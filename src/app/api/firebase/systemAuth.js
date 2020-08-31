40b3fa0f74eb38265cec0668ed50833d3391f4ff
rakzhodekams <i00nsu@cryptolab.org>
2020-08-31 17:19:03 +0100

    Firebase Auth Data

---
+++ src/app/api/firebase/systemAuth.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmCG4ViU9rjN7gE7nnSSaW-AIJJo9aQPc",
+    authDomain: "fireup-c1159.firebaseapp.com",
+    databaseURL: "https://fireup-c1159.firebaseio.com",
+    projectId: "fireup-c1159",
+    storageBucket: "fireup-c1159.appspot.com",
+    messagingSenderId: "354260595930",
+    appId: "1:354260595930:web:693b9428d65d7ade09a1e3",
+    measurementId: "G-3ZSWQJQH95"
+};
+
+module.exports.firebaseConfig = firebaseConfig;
