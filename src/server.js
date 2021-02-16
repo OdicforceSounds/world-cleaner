fa303f48037bfc4d30bee54e25a706c018627032
Rakzhodekams <i00nsu@cryptolab.net>
2021-02-16 16:57:09 +0000

    files moved to /testing/

--- src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require("express");
-var app = express();
-app.get("/", function (req, res) {
-  app.listen(PORT, function () {
-    console.log("Server is running at port: " + PORT);
-
-  });
-});
