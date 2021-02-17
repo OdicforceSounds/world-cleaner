34f55f022b7035f382db9b8915818b9911a1c9ef
Rakzhodekams <i00nsu@cryptolab.net>
2021-02-17 17:08:41 +0000

    using Express for routing - everything working as expected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-// Express
-var express = require("express");
-var app = express();
+// Import ExpressJS
+const express = require("express");
+const app = express();
+// Start Local HTTP Server
+app.listen(3000);
+
+// Start Web page application
+// Start root page 
+app.get("/", (req, res) => {
+  res.sendFile('./index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/About/about.html', {root: __dirname});
+});
+
+app.get('/docs', (req, res) => {
+  res.sendFile('./views/Documents/docs.html', {root: __dirname});
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile('./views/Login/login.html', {root: __dirname});
+});
+
+app.get('/registration', (req, res) => {
+  res.sendFile('./views/Registration/Company/company.html', {root: __dirname});
+});
+
+app.get('/App', (req, res) => {
+  res.sendFile('./views/App/App.html', {root: __dirname});
+});
+
+app.get('/error', (req, res) => {
+  res.status(404).sendFile('./views/404/404.html', {root: __dirname});
+});
+
 // Firebase 
 const {
   firebase,
@@ -9,11 +42,12 @@ const {
   fireAdmin
 } = require("./services/fireup");
 
-//console.log(app); // active to see full object in console 
-console.log(firebase, fireAuth, fireStore, fireAdmin);
+// active to see full object in console 
+// notes/ folder
+//console.log(firebase);
+//console.log(fireAuth);
+//console.log(fireStore);
+//console.log(fireAdmin);
 
-app.get("/", function (req, res) {
-  app.listen(PORT, function () {
-    console.log("Server is running at port: " + PORT);
-  });
-});
+
+
