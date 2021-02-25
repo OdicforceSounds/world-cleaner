eafd5464d5ddfcd20638eae2240a7be69007e542
Rakzhodekams <i00nsu@cryptolab.net>
2021-02-25 03:44:57 +0000

    as simples as possible

--- src/index.js
+++ src/index.js
@@ -1,53 +1,67 @@
-// Import ExpressJS
-const express = require("express");
-const app = express();
-// Start Local HTTP Server
-app.listen(3000);
+// DOM elements
+const guideList = document.querySelector('.guides');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+const adminItems = document.querySelectorAll('.admin');
 
-// Start Web page application
-// Start root page 
-app.get("/", (req, res) => {
-  res.sendFile('./index.html', {root: __dirname});
+const setupUI = (user) => {
+  if (user) {
+    if (user.admin) {
+      adminItems.forEach(item => item.style.display = 'block');
+    }
+    // account info
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
+      `;
+      accountDetails.innerHTML = html;
+    });
+    // toggle user UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // clear account info
+    accountDetails.innerHTML = '';
+    // toggle user elements
+    adminItems.forEach(item => item.style.display = 'none');
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
+
+// setup guides
+const setupGuides = (data) => {
+
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html
+  } else {
+    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
+  }
+  
+
+};
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
 });
-
-app.get('/about', (req, res) => {
-  res.sendFile('./views/About/about.html', {root: __dirname});
-});
-
-app.get('/docs', (req, res) => {
-  res.sendFile('./views/Documents/docs.html', {root: __dirname});
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile('./views/Login/login.html', {root: __dirname});
-});
-
-app.get('/registration', (req, res) => {
-  res.sendFile('./views/Registration/Company/company.html', {root: __dirname});
-});
-
-app.get('/App', (req, res) => {
-  res.sendFile('./views/App/App.html', {root: __dirname});
-});
-
-app.get('/error', (req, res) => {
-  res.status(404).sendFile('./views/404/404.html', {root: __dirname});
-});
-
-// Firebase 
-const {
-  firebase,
-  fireAuth,
-  fireStore,
-  fireAdmin
-} = require("./services/fireup");
-
-// active to see full object in console 
-// notes/ folder
-//console.log(firebase);
-//console.log(fireAuth);
-//console.log(fireStore);
-//console.log(fireAdmin);
-
-
-
